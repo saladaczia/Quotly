5bbec696b1d269c5a1350b37a6efa17ecb10837b
Maciej Sołoducha <biuro@goldenapp.pl>
2025-09-10 14:12:42 +0200

    Initial Commit

---
+++ Quotly/QuotlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotlyApp.swift
+//  Quotly
+//
+//  Created by Maciej Sołoducha on 10/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct QuotlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
